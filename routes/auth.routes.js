504482b3eb030d57cb2c779ca58d883510635da5
Akshay patra <akshaykumarpatra00@gmail.com>
2026-02-26 23:15:59 +0530

    Remove unused jwt and PrismaClient imports
    Removed unused imports for jwt and PrismaClient.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const { PrismaClient } = require("@prisma/client");
-
+const jwt = require
 const prisma = new
 
 // POST /auth/login
